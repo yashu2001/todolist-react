51167ddcb26a626af3161286bb5e03e5707cf8a5
yashu2001 <y_narang@yahoo.in>
2020-04-20 22:28:16 +0530

    adding initial files

---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Todos extends Component {
+    render() {
+        return (
+            <div>
+                Hello this is the todos Component
+            </div>
+        )
+    }
+}
+
+export default Todos
